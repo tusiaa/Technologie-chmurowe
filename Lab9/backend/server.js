74545be11bd461d8fa9ea7c845240b32be5ac2d8
tusiaa <sandra.leman146@gmail.com>
2022-06-17 20:44:45 +0200

    k8s

---
+++ Lab9/backend/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PGPORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const Redis = require("ioredis");
+
+const dbConnData = {
+  port: process.env.REDIS_PORT || 6379,
+  host: process.env.REDIS_HOST || 'redis',
+};
+const client = new Redis(dbConnData);
+
+client.on('error', err => {
+  console.error('Error connecting to Redis', err);
+});
+client.on('connect', () => {
+    console.log(`Connected to Redis.`)
+    app.listen(PORT, () => {
+      console.log(`API server listening at http://localhost:${PORT}`);
+    });
+});
